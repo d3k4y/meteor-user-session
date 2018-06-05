64beee997062ec1205d7729cbfee788fd0443476
Daniel Kloeters <dkloeters@hax-solutions.de>
2018-06-05 21:37:12 +0200

    shortened summary

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: "d3k4y:user-session",
-    summary: "Provides a UserSession object that works just like Session does, except it's persistent so you can preserve state across devices *and* sessions.",
+    summary: "Provides a UserSession object that works just like Session does, except it's persistent",
     version: "0.3.0",
     git: "https://github.com/d3k4y/meteor-user-session.git",
 });
